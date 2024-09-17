0580fe2755a013623cc1b1469c6a3a728226a7fc
Riceball LEE <snowyu.lee@gmail.com>
2024-09-17 17:13:08 +0800

    refactor: use @isdk/bash-parser package

--- src/lib/valid-shell-cmd.ts
+++ src/lib/valid-shell-cmd.ts
@@ -1,4 +1,4 @@
-import { parse as parseShellCommand } from '@ein/bash-parser';
+import { parse as parseShellCommand } from '@isdk/bash-parser';
 
 export async function validShellCmd(cmd: string) {
   const ast = await parseShellCommand(cmd, {insertLOC: true});
